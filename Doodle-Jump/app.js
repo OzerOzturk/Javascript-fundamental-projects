65da938fe0876ed1626f49133975d06134825c5c
OzerOzturk <ozerozturk1636@gmail.com>
2021-04-21 23:24:36 +0300

    creating platform class and see these platform at random on the screen at this stage.

--- Doodle-Jump/app.js
+++ Doodle-Jump/app.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () =>{
     let doodlerLeftSpace = 50
     let doodlerBottomSpace = 150
     let isGameOver = false
+    let platformCount = 5
+    let platforms = []
 
     function createDoodler() {
         grid.appendChild(doodler)
@@ -13,12 +15,39 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     }
 
+    class Platform {
+        constructor (newPlatBottom){
+            this.bottom = newPlatBottom
+            this.left = Math.random() * 315
+            this.visual = document.createElement('div')
+
+            const visual = this.visual
+            visual.classList.add('platform')
+            visual.style.left = this.left + 'px'
+            visual.style.bottom = this.bottom + 'px'
+            grid.appendChild(visual)
+        }
+    }
+
+    function createPlatforms(){
+        for (let i=0; i<platformCount; i++){
+            let platGap = 600 / platformCount 
+            let newPlatBottom = 100 + i * platGap
+            let newPlatform = new Platform (newPlatBottom)
+            platforms.push(newPlatform)
+            console.log(platforms);
+        }
+    }
+
+
+
     function start(){
         if(!isGameOver) {
             createDoodler()
+            createPlatforms()
         }
     }
-    //attach to button 
+    //attach t o button 
     start()
 
     
