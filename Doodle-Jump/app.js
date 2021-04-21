b8d427fc1a1ce26b62fa6f2387e50249e258c9b0
OzerOzturk <ozerozturk1636@gmail.com>
2021-04-21 22:47:52 +0300

    creating doodler with const,let and its function.

---
+++ Doodle-Jump/app.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const grid =document.querySelector('.grid')
+    const doodler = document.createElement('div')
+    let doodlerLeftSpace = 50
+    let doodlerBottomSpace = 150
+    let isGameOver = false
+
+    function createDoodler() {
+        grid.appendChild(doodler)
+        doodler.classList.add('doodler')
+        doodler.style.left = doodlerLeftSpace + 'px'
+        doodler.style.bottom = doodlerBottomSpace + 'px'
+
+    }
+
+    function start(){
+        if(!isGameOver) {
+            createDoodler()
+        }
+    }
+    //attach to button 
+    start()
+
+    
+} )
